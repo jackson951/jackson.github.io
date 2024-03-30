5ecdd10af41a89034294cc803be677bcf22ec3f2
jackson951 <127627309+jackson951@users.noreply.github.com>
2024-03-30 20:58:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,14 @@ document.addEventListener('DOMContentLoaded', function() {
       },
     });
   });
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('change', function () {
+  if (this.checked) {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = 'none';
+  }
+});
