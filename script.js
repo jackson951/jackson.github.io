cf2d9109858de7b67b547ab7906bee95f0cc0732
jackson951 <127627309+jackson951@users.noreply.github.com>
2024-06-29 17:12:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const fullname=document.getElementById("name");
 const email=document.getElementById("email");
 const subject=document.getElementById("subject");
 const message=document.getElementById("message");
+//From : "jacksonkhuto591@gmail.com",(original)
 
 function sendEmail(){
 
@@ -11,7 +12,7 @@ function sendEmail(){
   Email.send({
     SecureToken : "e1ec463a-dccc-4b15-bc1d-70818f3f807e ",
     To : "jacksonkhuto591@gmail.com",
-    From : "jacksonkhuto591@gmail.com",
+    From : email,
     Subject : subject.value,
     Body : bodyMessage
 }).then(
